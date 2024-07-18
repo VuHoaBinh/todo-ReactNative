dc18c72352792ed5819b1a6e8c616c769a3fbf07
VuHoaBinh <hoabinh.vippro63@gmail.com>
2024-07-18 15:57:58 +0700

    fix: Gui

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
-import { Image, StyleSheet, View, Text } from "react-native";
-
+import { Image, StyleSheet, View, Text, ImageBackground } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 import { StatusBar } from "expo-status-bar";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
 import StartGameScreen from "@/screens/StartGameScreens";
@@ -9,22 +9,31 @@ export default function HomeScreen() {
     <ParallaxScrollView
       headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
       headerImage={
-        <Image
+        <ImageBackground
           source={require("@/assets/images/partial-react-logo.png")}
           style={styles.reactLogo}
         />
       }
     >
-      <View>
+      <LinearGradient
+        colors={["#4c669f", "#3b5998", "#192f6a"]}
+        style={styles.linearGradient}
+      >
         <Text style={styles.title}>Hello World!</Text>
         <StatusBar style="auto" />
         <StartGameScreen />
-      </View>
+      </LinearGradient>
     </ParallaxScrollView>
   );
 }
 
 const styles = StyleSheet.create({
+  linearGradient: {
+    flex: 1,
+    paddingLeft: 5,
+    paddingRight: 5,
+    borderRadius: 5,
+  },
   title: {
     color: "#FFFFFF",
   },
@@ -39,9 +48,10 @@ const styles = StyleSheet.create({
   },
   reactLogo: {
     height: 178,
-    width: 290,
+    width: 390,
     bottom: 0,
     left: 0,
     position: "absolute",
+    opacity: 0.25,
   },
 });

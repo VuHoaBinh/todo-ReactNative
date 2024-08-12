d2009b6f72b6301049a029d0a4eeaa71f4545f17
VuHoaBinh <hoabinh.vippro63@gmail.com>
2024-08-12 16:40:23 +0700

    fix: navigation

--- screens/NewScreens.tsx
+++ screens/NewScreens.tsx
@@ -5,13 +5,30 @@ import HomeTab from "@/tab/HomeTab";
 import SettingTab from "@/tab/SettingTab";
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
+import { Ionicons } from "@expo/vector-icons";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 function ExpenseOverview() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === "Home") {
+            iconName = focused ? "home" : "home-outline";
+          } else if (route.name === "Settings") {
+            iconName = focused ? "settings" : "settings-outline";
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "tomato",
+        tabBarInactiveTintColor: "gray",
+      })}
+    >
       <Tab.Screen name="Home" component={HomeTab} />
       <Tab.Screen name="Settings" component={SettingTab} />
     </Tab.Navigator>
@@ -20,11 +37,15 @@ function ExpenseOverview() {
 
 function NewScreens() {
   return (
-    <View>
+    <View style={{ flex: 1 }}>
       <StatusBar style="light" />
       <NavigationContainer independent={true}>
         <Stack.Navigator>
-          <Stack.Screen name="ExpenseOverview" component={ExpenseOverview} />
+          <Stack.Screen
+            name="ExpenseOverview"
+            component={ExpenseOverview}
+            options={{ headerShown: false }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </View>
